1ae9714abe37c58c5c7b46be1afb83f97a1f1524
zongtong009 <zongtongguilai1@gmail.com>
2021-03-26 16:42:30 +0800

    1

---
+++ python/GLLPrename1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import os
+
+files = os.listdir(".") #open the current folder
+
+
+for file in files:
+    portion = os.path.splitext(file)
+    # 更改exb文件名,GLLD改为GLLP
+    if portion[1] == r".exb" and "GLLD" in portion[0]:  
+    # 重新组合文件名和后缀名
+        newname = "GLLP" + portion[0][4:] +".exb"
+        os.rename(file,newname)
+            
